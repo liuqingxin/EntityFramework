a3b1ccf37d485fc5c17012d914d6803f849d5dce
liuqingxin <liuqingxin@yun-kai.com>
2016-02-02 15:38:33 +0800

    Teset

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OracleCodeFirst.Models;
+using OracleCodeFirst;
+using System.Linq;
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+
+        [TestMethod]
+        public void TestOracle()
+        {
+            OracleContext context = new OracleContext();
+            var data = context.Test.FirstOrDefault();
+            
+        }
+    }
+}
